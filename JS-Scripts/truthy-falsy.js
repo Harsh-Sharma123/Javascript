e084bc6feeeed4eefed2139beaaba2f53daae769
Harsh Sharma <hs804506@gmail.com>
2024-05-31 22:42:11 +0530

    Truthy Falsy Values Code Example

---
+++ JS-Scripts/truthy-falsy.js
@@ -0,0 +1,13 @@
+let firstName = undefined;
+if(firstName){
+    console.log("FirstName is not empty!");
+}else{
+    console.log("FirstName is empty!");
+}
+
+let name = "Harsh";
+if(name){
+    console.log("FirstName is not empty!");
+}else{
+    console.log("FirstName is empty!");
+}
