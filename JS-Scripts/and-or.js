89c929be6643deb0c2a39812850ce2c08f91705c
Harsh Sharma <hs804506@gmail.com>
2024-06-08 21:51:32 +0530

    Code for and or operator

---
+++ JS-Scripts/and-or.js
@@ -0,0 +1,8 @@
+let firstName = "Harsh";
+let age = 23;
+
+if(firstName[0] === 'H' && age > 18){
+    console.log("Firstname have first character H and also age is greater than 18!");
+}else{
+    console.log("Neither Firstname's first character is 'H' nor age is greater than 18!");
+}
