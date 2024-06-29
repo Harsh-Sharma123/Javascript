e716a0d51767596c6167e17933496cc31a2d8726
Harsh Sharma <hs804506@gmail.com>
2024-06-29 11:43:00 +0530

    Continue Keyword

--- JS-Scripts/break-continue.js
+++ JS-Scripts/break-continue.js
@@ -3,3 +3,9 @@ for (let i=0;i<=10;i++){
     if(i == 4) break;
     console.log(i);
 }
+
+console.log("Example of Continue : ");
+for (let i=0;i<=10;i++){
+    if(i == 4) continue;
+    console.log(i);
+}
