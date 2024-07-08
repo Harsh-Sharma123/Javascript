7570a7e98825343787c9c75860493ffdb01e20a7
Harsh Sharma <hs804506@gmail.com>
2024-07-08 21:20:28 +0530

    Methods in Arrays

--- JS-Scripts/arrays.js
+++ JS-Scripts/arrays.js
@@ -16,3 +16,23 @@ console.log(typeof obj);
 console.log(typeof fruits);
 console.log(Array.isArray(fruits));
 console.log(Array.isArray(obj));
+
+// Push Method
+console.log("Before Push : ", fruits);
+fruits.push("orange");
+console.log("After Push : ", fruits);
+
+// Pop Method
+let poppedFruit = fruits.pop();
+console.log("Popped Fruit : ", poppedFruit);
+console.log("After Popping : ", fruits);
+
+// Unshift Method
+console.log("Before Unshift : ", fruits);
+fruits.unshift("Myfruit");
+console.log("After Unshift : ", fruits);
+
+// Shift Method
+console.log("Before Shift : ", fruits);
+fruits.shift();
+console.log("After Shift : ", fruits);
