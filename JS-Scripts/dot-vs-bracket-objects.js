2b04acfa6c80def6c5da86e5c9da9bffd80afd8b
Harsh Sharma <hs804506@gmail.com>
2024-07-22 22:05:32 +0530

    dot vs brackets in objects

---
+++ JS-Scripts/dot-vs-bracket-objects.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Harsh",
+    age: 22
+};
+
+person["personal hobbies"] = ["games", "games"];
+
+console.log(person.name);
+console.log(person.age);
+
+console.log(person["personal hobbies"]);
