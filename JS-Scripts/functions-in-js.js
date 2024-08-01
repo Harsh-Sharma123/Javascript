2dc4f9bd9413f9dd2c806eb6f8edb6aa7c31f23e
Harsh Sharma <hs804506@gmail.com>
2024-08-01 21:47:35 +0530

    functions declarations

--- JS-Scripts/functions-in-js.js
+++ JS-Scripts/functions-in-js.js
@@ -5,3 +5,30 @@ function happyBirthday(){
 happyBirthday();
 
 happyBirthday();
+
+function sumThreeNumber(num1, num2, num3){
+    return num1 + num2 + num3;
+}
+
+console.log(sumThreeNumber(1, 2,3 ));
+
+function isEven(num){
+    return num%2===0?"Even Number" : "Odd Number";
+}
+
+console.log(isEven(10));
+
+function firstChar(str){
+    return str[0];
+}
+
+console.log("First Character of Hello : ", firstChar("Hello"));
+
+function findTarget(arr, target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===target) return "Found";
+    }
+    return "Not Found";
+}
+
+console.log(findTarget([1, 2, 3, 4, 5], 5));
