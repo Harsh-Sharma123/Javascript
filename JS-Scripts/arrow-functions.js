90f49e623e4d3e253aab755c4d577dc47f5ea30a
Harsh Sharma <hs804506@gmail.com>
2024-08-08 22:52:13 +0530

    arrow function

---
+++ JS-Scripts/arrow-functions.js
@@ -0,0 +1,10 @@
+const singHappyBirthday = () => {
+    console.log("Happy Birthday To You !!!!");
+}
+
+singHappyBirthday();
+
+const isEven = number => number %  2 === 0;
+
+console.log(isEven(10));
+console.log(isEven(7));
