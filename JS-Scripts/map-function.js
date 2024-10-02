44b60122bad64690c19d5b6eded35ed532059c7c
Harsh Sharma <hs804506@gmail.com>
2024-10-02 11:49:18 +0530

    map function in js

---
+++ JS-Scripts/map-function.js
@@ -0,0 +1,22 @@
+const numbers = [1, 2, 3, 4];
+
+const square = function (number){
+    return number*number;
+}
+
+const squareNumbers = numbers.map(square);
+
+console.log(squareNumbers);
+
+const users = [
+    {firstName : 'A', age: 20},
+    {firstName : 'B', age: 21},
+    {firstName : 'C', age: 22},
+    {firstName : 'D', age: 23}
+];
+
+const firstNames = users.map((user)=>{
+    return user.firstName;
+});
+
+console.log(firstNames);
