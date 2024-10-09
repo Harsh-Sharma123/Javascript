00455fee3c04d13e87623da8e8046d81743a20ef
Harsh Sharma <hs804506@gmail.com>
2024-10-09 22:18:32 +0530

    Maps in JS

---
+++ JS-Scripts/map.js
@@ -0,0 +1,10 @@
+// Maps
+// Map is iterable
+
+// store data in ordered fashion
+
+// store key-value pair like objects
+// duplicate keys are not allowed like objects
+
+// Difference between Objects and Maps
+// Objects can only have strings or symbols as keys while maps can have anything as key liek array, number, string
